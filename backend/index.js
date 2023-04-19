8628ddc3b0ab1a74717877c003cca6efb1212fb8
mit-anuj <anujmittal0040@gmail.com>
2023-04-19 12:58:18 +0530

    we saved some data in the user collection in the database

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const connectToDB = require('./db.js')
 
 //* this will connect to the database.
 connectToDB();
+app.use(express.urlencoded({ extended:true}))
+app.use(express.json())
 
 app.use('/api/users',require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router()
+const User = require('../models/User')
+
+
 
 router.get('/', (req, res) => {
-    res.send('welcome to auth');
+    // res.send('welcome to auth');
+    // res.send(req.body)
+    // ! here we are basically creating an instance of User and passing the data from request to create an object in the database.
+    const user = new User(req.body)
+    //* we are saving the data so that it will reflect the changes in the database.
+    user.save()
 })
 
 module.exports = router;
