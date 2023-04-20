65de7d54675041d2bf486b978eaee23441c293af
mit-anuj <anujmittal0040@gmail.com>
2023-04-21 01:18:29 +0530

    added an end point to update the existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -6,7 +6,7 @@ const { validationResult, body } = require('express-validator');
 
 
 
-// ROUTE 1: (GET) get all the notes of the user
+// ROUTE 1: (GET) get all the notes of the user  Login required
 router.get('/fetchallnotes', fetchUser, async (req, res) => {
     try {
         // we will get the user id form the middleware.
@@ -22,7 +22,7 @@ router.get('/fetchallnotes', fetchUser, async (req, res) => {
 })
 
 
-// ROUTE 2: (POST) create a new note.
+// ROUTE 2: (POST) create a new note.  Login required
 router.post('/newnote', [
     body('title', 'Email length should be 10 to 30 characters')
         .isLength({ min: 3 }),
@@ -45,6 +45,45 @@ router.post('/newnote', [
         const savedNote = await notes.save()
 
         res.json(savedNote)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({
+            error: 'internal server error'
+        })
+
+    }
+})
+
+// Route 3: (PATCH) Update an existing note  Login required 
+router.patch('/updatenote/:id', fetchUser, async (req, res) => {
+    try {
+        
+        // destructing the object that we received thorugh the req body
+        const { title, description, tag } = req.body;
+        // only adding those values to the object that we received thorugh the req.
+        const newNote= {};
+        if(title){
+            newNote.title = title;
+        }
+        if(description){
+            newNote.description = description;
+        }
+        if(tag){
+            newNote.tag = tag;
+        }
+        // here we are checking whether that particular note exists in the database or not.
+        const note = await Notes.findById(req.params.id)
+        if(!note){
+            return res.status(404).json({error: 'note not found'})
+        }
+        //! checking whether the logged in user is allowed to update that particular note or not.
+        if(note.user.toString() !== req.user.id){
+            return res.send('not allowed')
+        }
+        //! if the user is verified then we are updating the note .
+        const updatenote = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+
+        res.json(updatenote)
     } catch (error) {
         console.error(error.message)
         res.status(500).json({
