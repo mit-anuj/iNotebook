89d47f14d16e1c51ab683c36145d74a039a59afa
mit-anuj <anujmittal0040@gmail.com>
2023-04-27 18:35:36 +0530

    added fontawesome icons

--- src/components/Notesitem.js
+++ src/components/Notesitem.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 function Notesitem(props) {
     return (
         <div className='col-md-3 my-3'>
@@ -7,6 +6,7 @@ function Notesitem(props) {
                 <div className="card-body">
                     <h5 className="card-title">{props.note.title}</h5>
                     <p className="card-text">{props.note.description}</p>
+                    <i className="fa-sharp fa-solid fa-trash"></i>
                 </div>
             </div>
         </div>
