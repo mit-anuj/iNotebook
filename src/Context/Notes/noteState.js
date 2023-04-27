19d09a7076658f52cf5ce0edac5b62cb782254ae
mit-anuj <anujmittal0040@gmail.com>
2023-04-27 23:35:00 +0530

    added delete note fuctionality

--- src/Context/Notes/noteState.js
+++ src/Context/Notes/noteState.js
@@ -8,7 +8,7 @@ const NoteState = (props) => {
             "_id": "64419a7645da3920c154d712",
             "user": "644035d16d85b013b0ca09eb",
             "title": "blog",
-            "description": "creating a new web app which will store the notes on the cloud",
+            "description": "creating a new web app which will store the notes on the cloud 1",
             "date": "2023-04-20T20:03:02.834Z",
             "__v": 0
         },
@@ -16,7 +16,7 @@ const NoteState = (props) => {
             "_id": "64419a7645da23920c154d712",
             "user": "644035d16d85b013b0ca09eb",
             "title": "blog",
-            "description": "creating a new web app which will store the notes on the cloud",
+            "description": "creating a new web app which will store the notes on the cloud 2",
             "date": "2023-04-20T20:03:02.834Z",
             "__v": 0
         },
@@ -24,7 +24,7 @@ const NoteState = (props) => {
             "_id": "64419a7645da43920c154d712",
             "user": "644035d16d85b013b0ca09eb",
             "title": "blog",
-            "description": "creating a new web app which will store the notes on the cloud",
+            "description": "creating a new web app which will store the notes on the cloud 3",
             "date": "2023-04-20T20:03:02.834Z",
             "__v": 0
         },
@@ -32,7 +32,7 @@ const NoteState = (props) => {
             "_id": "64419a76145da3920c154d712",
             "user": "644035d16d85b013b0ca09eb",
             "title": "blog",
-            "description": "creating a new web app which will store the notes on the cloud",
+            "description": "creating a new web app which will store the notes on the cloud 4",
             "date": "2023-04-20T20:03:02.834Z",
             "__v": 0
         },
@@ -40,7 +40,7 @@ const NoteState = (props) => {
             "_id": "64419a76452da3920c154d712",
             "user": "644035d16d85b013b0ca09eb",
             "title": "blog",
-            "description": "creating a new web app which will store the notes on the cloud",
+            "description": "creating a new web app which will store the notes on the cloud 5",
             "date": "2023-04-20T20:03:02.834Z",
             "__v": 0
         },
@@ -49,7 +49,6 @@ const NoteState = (props) => {
 
     // Add note
     const addNote = (title,description,tag)=>{
-        console.log("heiodijfadj")
         let note={
             "_id": "64419a76452da39420c154d712",
             "user": "644035d16d85b013b0ca09eb",
@@ -63,8 +62,12 @@ const NoteState = (props) => {
     }
 
     // Delete note
-    const deleteNote = ()=>{
-
+    const deleteNote = (id)=>{
+        // using filter we can find the note with the given id and filter function will return a new array withour that note.
+        const newNotes =notes.filter((note)=>{
+            return note._id!==id
+        })
+        setNotes(newNotes)
     }
 
     // edit note
@@ -72,7 +75,7 @@ const NoteState = (props) => {
 
     }
     return (
-        <NoteContext.Provider value={{notes,setNotes,addNote}}>
+        <NoteContext.Provider value={{notes,setNotes,addNote, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
--- src/components/Notesitem.js
+++ src/components/Notesitem.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import NoteContext from '../Context/Notes/noteContext'
+
 function Notesitem(props) {
+    const {note}= props
+    const context = useContext(NoteContext)
+    const {deleteNote}= context;
     return (
         <div className='col-md-3 my-3'>
             <div className="card " >
                 <div className="card-body">
-                    <h5 className="card-title">{props.note.title}</h5>
-                    <p className="card-text">{props.note.description}</p>
-                    <i className="fa-sharp fa-solid fa-trash mx-2"></i>
+                    <h5 className="card-title">{note.title}</h5>
+                    <p className="card-text">{note.description}</p>
+                    {/* we are adding an onclick event listener to the trash icon so that it can delete the respective note. */}
+                    <i className="fa-sharp fa-solid fa-trash mx-2" onClick={()=>{return deleteNote(note._id)}}></i>
                     <i className="fa-solid fa-pen-to-square mx-2"></i>
                     {props.note.note}
                 </div>
