93f6ec29b00afe28aeb3468145216a87afd198ed
mit-anuj <anujmittal0040@gmail.com>
2023-04-28 13:33:12 +0530

    now we are fetching the notes form the api and also adding notes in the db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const connectToDB = require('./db.js')
+const cors =require('cors')
 // react will be connecting on the 3000 port so we will be using 5000 for express.
 const port = 5000;
 
@@ -8,6 +9,7 @@ const port = 5000;
 connectToDB();
 app.use(express.urlencoded({ extended:true}))
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/notes',require('./routes/notes.js'))
 app.use('/api/auth', require('./routes/auth'))
